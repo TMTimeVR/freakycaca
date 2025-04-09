8ba61a75c7d5f0b7f77764e180e95c464af7bdab
TMTime <108794834+TMTimeVR@users.noreply.github.com>
2025-04-09 16:50:47 +0200

    Update caca.lua

--- caca.lua
+++ caca.lua
@@ -1,154 +1,77 @@
 repeat task.wait() until game:IsLoaded()
-shared.oldgetcustomasset = shared.oldgetcustomasset or getcustomasset
-task.spawn(function()
-    repeat task.wait() until shared.VapeFullyLoaded
-    getgenv().getcustomasset = shared.oldgetcustomasset
-end)
-
-local CheatEngineMode = false
-if (not getgenv) or (getgenv and type(getgenv) ~= "function") then CheatEngineMode = true end
-if getgenv and not getgenv().shared then CheatEngineMode = true; getgenv().shared = {}; end
-if getgenv and not getgenv().debug then CheatEngineMode = true; getgenv().debug = {traceback = function(string) return string end} end
-if getgenv and not getgenv().require then CheatEngineMode = true; end
-if getgenv and getgenv().require and type(getgenv().require) ~= "function" then CheatEngineMode = true end
-
-local debugChecks = {
-    Type = "table",
-    Functions = {
-        "getupvalue",
-        "getupvalues",
-        "getconstants",
-        "getproto"
-    }
-}
-
-local function checkExecutor()
-    if identifyexecutor ~= nil and type(identifyexecutor) == "function" then
-        local suc, res = pcall(function()
-            return identifyexecutor()
-        end)   
-        local blacklist = {'solara', 'cryptic', 'xeno'}
-        local core_blacklist = {'solara', 'xeno'}
-        if suc then
-            for i,v in pairs(blacklist) do
-                if string.find(string.lower(tostring(res)), v) then CheatEngineMode = true end
-            end
-            for i,v in pairs(core_blacklist) do
-                if string.find(string.lower(tostring(res)), v) then
-                    pcall(function()
-                        getgenv().queue_on_teleport = function() warn('queue_on_teleport disabled!') end
-                    end)
-                end
-            end
-        end
-    end
-end
-task.spawn(function() pcall(checkExecutor) end)
-
-local function checkRequire()
-    if CheatEngineMode then return end
-    local bedwarsID = {
-        game = {6872274481, 8444591321, 8560631822},
-        lobby = {6872265039}
-    }
-    if table.find(bedwarsID.game, game.PlaceId) then
-        repeat task.wait() until game:GetService("Players").LocalPlayer.Character
-        repeat task.wait() until game:GetService("Players").LocalPlayer.PlayerGui and game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("TopBarAppGui")
-        local suc, data = pcall(function()
-            return require(game:GetService("ReplicatedStorage").TS.remotes).default.Client
-        end)
-        if (not suc) or type(data) ~= 'table' or (not data.Get) then CheatEngineMode = true end
-    end
-end
-
-local function checkDebug()
-    if CheatEngineMode then return end
-    if not getgenv().debug then 
-        CheatEngineMode = true 
-    else 
-        if type(debug) ~= debugChecks.Type then 
-            CheatEngineMode = true
-        else 
-            for _, v in pairs(debugChecks.Functions) do
-                if not debug[v] or (debug[v] and type(debug[v]) ~= "function") then 
-                    CheatEngineMode = true 
-                else
-                    local suc, res = pcall(debug[v]) 
-                    if tostring(res) == "Not Implemented" then 
-                        CheatEngineMode = true 
-                    end
-                end
-            end
-        end
-    end
-end
-if not CheatEngineMode then checkDebug() end
 
 local RiseMode = true
 local baseDirectory = RiseMode and "rise/" or "vape/"
+
 if not isfolder('vape') then makefolder('vape') end
 if not isfolder('rise') then makefolder('rise') end
-shared.CheatEngineMode = shared.CheatEngineMode or CheatEngineMode
-
-local isfile = isfile or function(file)
-	local suc, res = pcall(function() return readfile(file) end)
-	return suc and res ~= nil
-end
-local delfile = delfile or function(file) writefile(file, "") end
 if not isfolder(baseDirectory) then makefolder(baseDirectory) end
 
-local VWFunctions = {}
-function VWFunctions.LogStats()
-    pcall(function()
-        local executor = identifyexecutor and identifyexecutor() or "Unknown"
-        local HWID = tostring(game:GetService("RbxAnalyticsService"):GetClientId())
-        local executors = {"solara", "fluxus", "macsploit", "hydrogen", "wave", "codex", "arceus", "delta", "vega", "cubix", "celery", "cryptic", "cacti", "appleware", "synapse", "salad"}
-        if identifyexecutor then
-            for _, v in pairs(executors) do
-                if string.find(string.lower(executor), v) then
-                    executor = v
-                    break
-                end
-            end
+local function install_profiles(num)
+    if not num then return warn("No number specified!") end
+    local guiprofiles = {}
+    local profilesfetched
+    local repoOwner = RiseMode and "VapeVoidware/RiseProfiles" or "Erchobg/VoidwareProfiles"
+    
+    local function downloadProfile(path)
+        local fullPath = baseDirectory..path
+        if not isfile(fullPath) then
+            local res = game:HttpGet('https://raw.githubusercontent.com/'..repoOwner..'/main/'..path)
+            writefile(fullPath, res)
         end
-        local headers = {
-            ["Content-type"] = "application/json",
-            ["Authorization"] = "Bearer imsureitwontgetddosed"
-        }
-        local data = {
-            ["client_id"] = HWID, 
-            ["executor"] = executor
-        }
-        local final_data = game:GetService("HttpService"):JSONEncode(data)
-        local url = "https://voidware-stats.vapevoidware.xyz/stats/data/add"
-        local a = request({
-            Url = url,
-            Method = 'POST',
-            Headers = headers,
-            Body = final_data
-        })
-        local statusCodes = {
-            ["403"] = "Voidware Error]: Error doing step2 Error code: 1986",
-            ["401"] = "Voidware Error]: Error doing step2 Error code: 1922",
-            ["429"] = "Voidware Error]: Error doing step2 Error code: 1954 Please rejoin!"
-        }
-        if a["StatusCode"] ~= 200 then 
-            if statusCodes[tostring(a["StatusCode"])] then 
-                warn(tostring(statusCodes[tostring(a["StatusCode"])])) 
-            else 
-                warn("Voidware Error]: Error doing step2 Error code: 1900") 
-            end 
+    end
+
+    local function fetchProfileList(path)
+        local url = "https://api.github.com/repos/"..repoOwner.."/contents/"..path
+        local res = game:HttpGet(url)
+        for _, v in pairs(game:GetService("HttpService"):JSONDecode(res)) do
+            if v.name then table.insert(guiprofiles, path.."/"..v.name) end
         end
-    end)
-end
-function VWFunctions.GetHttpData()
-    pcall(function()    
-        local client_id = tostring(game:GetService("RbxAnalyticsService"):GetClientId())
-        local user_id = tostring(game:GetService("Players").LocalPlayer.UserId)
-        local voidware_id = "github"
-        return voidware_id, user_id, client_id
-    end)
+    end
+
+    if num == 1 then fetchProfileList("Profiles") end
+    if num == 2 then fetchProfileList("ClosetProfiles") end
+    repeat task.wait() until guiprofiles[1]
+    for _, name in pairs(guiprofiles) do
+        downloadProfile(name)
+    end
+
+    if not isfolder(baseDirectory.."Libraries") then makefolder(baseDirectory.."Libraries") end
+    if num == 1 then writefile(baseDirectory.."Libraries/profilesinstalled3.txt", "true") end
+    if num == 2 then writefile(baseDirectory.."ClosetProfiles/profilesinstalled3.txt", "true") end
 end
 
-shared.VWFunctions = VWFunctions
-getgenv().VWFunctions = VWFunctions
+local function are_installed_1()
+    return isfile(baseDirectory.."Libraries/profilesinstalled3.txt")
+end
+
+local function are_installed_2()
+    return isfile(baseDirectory.."ClosetProfiles/profilesinstalled3.txt")
+end
+
+if not are_installed_1() then install_profiles(1) end
+if not are_installed_2() then install_profiles(2) end
+
+-- Grab the latest commit
+local url = RiseMode and "https://github.com/VapeVoidware/VWRise/" or "https://github.com/VapeVoidware/VoidwareBakup"
+local commit = "main"
+for _, line in pairs(game:HttpGet(url):split("\n")) do
+    if line:find("commit") and line:find("fragment") then
+        local str = line:split("/")[5]
+        commit = str:sub(1, str:find('"') - 1)
+        break
+    end
+end
+
+writefile(baseDirectory.."commithash2.txt", commit)
+
+if ((not isfile(baseDirectory.."commithash.txt")) or (readfile(baseDirectory.."commithash.txt") ~= commit or commit == "main")) then
+    for _, path in pairs({
+        "Universal.lua", "MainScript.lua", "GuiLibrary.lua"
+    }) do
+        local full = baseDirectory..path
+        if isfile(full) and readfile(full):find("--This watermark is used to delete the file") then
+            delfile(full)
+        end
+    end
+end
